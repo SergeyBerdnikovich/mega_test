ab7561fb338e113172cede94e59d1210ed57bb7f
Sergey Berdnikovich <sergey.berdnikovich@productmadness.com>
2015-09-03 12:36:51 +0300

    Added capistrano, unicorn gems, capistrano, unicorn, nginx configs

--- Gemfile
+++ Gemfile
@@ -26,8 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
-# Use Unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -40,8 +39,9 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'capistrano', '3.4.0'
+  gem 'capistrano-rvm'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
 end
-
---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'mega_test'
+set :repo_url, 'git@github.com:SergeyBerdnikovich/mega_test.git'
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :deploy_user, 'atom'
+set :deploy_to, "/var/www/apps/#{fetch(:application)}"
+set :shared_path, "#{fetch(:deploy_to)}/shared"
+set :conditionally_migrate, true
+set :rvm_type, :user
+set :rvm_ruby_version, "2.2.1@#{fetch(:application)}"
+set :keep_releases, 6
+set :scm, :git
+set :log_level, :debug
+
+set(:config_files, %w(
+  database.example.yml
+  secrets.example.yml
+  unicorn.rb
+  unicorn_init.sh
+  nginx.conf
+))
+set(:symlinks, [
+  {
+    source: "#{fetch(:shared_path)}/config/nginx.conf",
+    link: "/etc/nginx/conf.d/default.conf"
+  },
+  {
+    source: "#{fetch(:shared_path)}/config/unicorn_init.sh",
+    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+  },
+  {
+    source: '/var/log/nginx/access.log',
+    link: "#{fetch(:shared_path)}/log/nginx.log"
+  },
+  {
+    source: '/var/log/nginx/error.log',
+    link: "#{fetch(:shared_path)}/log/nginx_error.log"
+  }
+])
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
+# Default value for :pty is false
+# set :pty, true
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+namespace :deploy do
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+  after 'deploy:setup_config', 'nginx:reload'
+  after 'deploy:publishing', 'unicorn:restart'
+  after :finishing, :cleanup
+end
